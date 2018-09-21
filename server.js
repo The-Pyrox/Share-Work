b06fcf9133831660ace013b27b4ddaa360a221d5
Ubuntu <ubuntu@ip-172-31-42-104.us-east-2.compute.internal>
2018-09-21 05:39:58 +0000

    Server up & Running

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = require('./app');
 
 const port = process.env.PORT || 9000;
 
-const server = http.createServer();
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello there, world!\n');
+});
 
 server.listen(port);
