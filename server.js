d90fb2aafae51051a37e1d209878ae8660f1e9a9
Poojan Rathod <poojanrathod@Poojans-MacBook-Air.local>
2018-09-21 10:58:52 +0530

    "Added server.js and app.js"

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const http = ('http');
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 9000;
+
+const server = http.createServer();
+
+server.listen(port);
